dfe5d1097235cd44e70392c5d9dd8728a260198e
evitan22 <kokravets@gmail.com>
2026-03-22 23:40:02 +0200

    changed users on students

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/users", {
+fetch("http://localhost:3000/students", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name: "Vasya", age: 19, id: 1 })
@@ -6,7 +6,7 @@ fetch("http://localhost:3000/users", {
 .then(response => response.json())
 .then(response => console.log(response, "response"));
 
-fetch("http://localhost:3000/users", {
+fetch("http://localhost:3000/students", {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name: "Olena", age: 85, id: 2 })
@@ -17,14 +17,14 @@ fetch("http://localhost:3000/users", {
 })
 .then(response => console.log(response, "response"));
 
-fetch("http://localhost:3000/users/1", {
+fetch("http://localhost:3000/students/1", {
     method: 'DELETE',
     headers: { 'Content-Type': 'application/json' },
 })
 .then(response => response.json())
 .then(response => console.log(response, "response"));
 
-fetch("http://localhost:3000/users/2", {
+fetch("http://localhost:3000/students/2", {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name: "Kyryl", age: 35, id: 2 })
@@ -33,7 +33,7 @@ fetch("http://localhost:3000/users/2", {
 .then(response => console.log(response, "response"));
 
 try {
-    fetch("http://localhost:3000/users")
+    fetch("http://localhost:3000/students")
     .then(response => response.json())
     .then(response => console.log(response, "response"))
 } catch (error) {
