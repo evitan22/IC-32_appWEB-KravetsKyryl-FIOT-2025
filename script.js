bb761143f89c07a5b8ae9f1ace9ee3eaf0f8d3c7
evitan22 <kokravets@gmail.com>
2026-03-22 17:55:42 +0200

    made lab1

---
+++ script.js
@@ -0,0 +1,41 @@
+fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: "Vasya", age: 19, id: 1 })
+})
+.then(response => response.json())
+.then(response => console.log(response, "response"));
+
+fetch("http://localhost:3000/users", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: "Olena", age: 85, id: 2 })
+})
+.then(response => {
+    console.log(response, response.status)
+    return response.json()
+})
+.then(response => console.log(response, "response"));
+
+fetch("http://localhost:3000/users/1", {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+})
+.then(response => response.json())
+.then(response => console.log(response, "response"));
+
+fetch("http://localhost:3000/users/2", {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name: "Kyryl", age: 35, id: 2 })
+})
+.then(response => response.json())
+.then(response => console.log(response, "response"));
+
+try {
+    fetch("http://localhost:3000/users")
+    .then(response => response.json())
+    .then(response => console.log(response, "response"))
+} catch (error) {
+    console.error("Помилка запиту:", error);
+}
